31b6220b9bf999109c2783c7745c1996a0487e20
Folusho Oladipo <fdoladipo@gmail.com>
2024-05-01 19:18:40 +0100

    MET-11: specify some env vars at the job level
    The env vars are:
    - NODE_ENV
    - RAILWAY_SERVICE_NAME

--- apps/next/app/server/db/schemaForEnv.ts
+++ apps/next/app/server/db/schemaForEnv.ts
@@ -3,12 +3,14 @@ import os from "os";
 export default function sqlSchemaForEnv(env: string) {
   const username = os.userInfo().username;
   switch (env) {
+    case "ci":
     case "development":
-      return `metaldev_${username}`;
     case "test":
       return `metaltest_${username}`;
+
     case "production":
       return "metalprod";
+
     default:
       throw new Error(`Unknown environment: ${env}`);
   }
