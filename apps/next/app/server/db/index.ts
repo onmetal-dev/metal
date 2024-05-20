1d000d9e4313ae651cfe77faf8ee03b93424136b
Rafael <rgarcia2009@gmail.com>
2024-05-20 12:14:26 -0400

    Raf/met 23 application applicationconfig data models (#34)
    * add application and applicationconfig tables
    
    * remove ca cert from connection (#32)

--- apps/next/app/server/db/index.ts
+++ apps/next/app/server/db/index.ts
@@ -2,12 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const options = {
-  ssl: {
-    ca: Buffer.from(process.env.POSTGRES_CA_DATA!, "base64").toString(),
-  },
-};
-
-export const db = drizzle(postgres(process.env.POSTGRES_URL!, options), {
+export const db = drizzle(postgres(process.env.POSTGRES_URL!), {
   schema,
 });
