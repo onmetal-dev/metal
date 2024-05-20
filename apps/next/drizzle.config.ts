f12735e08e744be67af352cfd445c5a9aa047759
Rafael <rgarcia2009@gmail.com>
2024-05-20 12:06:56 -0400

    remove ca cert from connection (#32)

--- apps/next/drizzle.config.ts
+++ apps/next/drizzle.config.ts
@@ -5,19 +5,7 @@ import { writeFileSync } from "fs";
 import sqlSchemaForEnv from "@/app/server/db/schemaForEnv";
 dotenv.config();
 
-// Config doesn't have a ca option, so we'll use a tmp file to store the ca
-const ca = tmp.fileSync();
-writeFileSync(
-  ca.name,
-  Buffer.from(process.env.POSTGRES_CA_DATA!, "base64").toString()
-);
-
 let connectionString = process.env.POSTGRES_URL!;
-if (connectionString.includes("?")) {
-  connectionString += `&sslrootcert=${ca.name}`;
-} else {
-  connectionString += `?sslrootcert=${ca.name}`;
-}
 
 export default {
   schema: "./app/server/db/schema.ts",
