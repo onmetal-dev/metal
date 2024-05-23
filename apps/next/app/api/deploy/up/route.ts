8b99fe7dfc11321fe786b17465cc271486abcec8
Folusho Oladipo <fdoladipo@gmail.com>
2024-05-23 23:54:13 +0100

    MET-20: generate both config.json and builders for users with custom Docker credentials

--- apps/next/app/api/deploy/up/route.ts
+++ apps/next/app/api/deploy/up/route.ts
@@ -1,19 +1,25 @@
 import { DIR_FOR_CLIENT_DEPLOYMENTS } from "@/app/server/util/constants";
+import { NixpackPlan } from "@/types/deployment";
 import { clerkClient } from "@clerk/nextjs";
 import { type NextRequest } from "next/server";
 import { exec as execCallbackBased, spawn } from "node:child_process";
-import { createWriteStream, existsSync, mkdirSync, writeFileSync } from "node:fs";
+import {
+  createWriteStream,
+  existsSync,
+  mkdirSync,
+  writeFileSync,
+} from "node:fs";
 import { Readable, Writable } from "node:stream";
-import { dirSync } from "tmp";
 import { promisify } from "node:util";
-import { NixpackPlan } from "@/types/deployment";
+import { dirSync } from "tmp";
 
 export const dynamic = "force-dynamic";
 
 const exec = promisify(execCallbackBased);
 const org = process.env.DOCKERHUB_ORG;
 const repo = process.env.DOCKERHUB_REPO;
-const cloudBuilderName = process.env.CLOUD_BUILDER_NAME;
+const builderLongName = process.env.CLOUD_BUILDER_LONG_NAME || "";
+const builderName = process.env.CLOUD_BUILDER_NAME || "";
 
 // This code below is heavily based on:
 // https://nextjs.org/docs/app/building-your-application/routing/route-handlers#streaming
@@ -22,21 +28,21 @@ const cloudBuilderName = process.env.CLOUD_BUILDER_NAME;
 function iteratorToStream(iterator: AsyncGenerator<Uint8Array, void, unknown>) {
   return new ReadableStream({
     async pull(controller) {
-      const { value, done } = await iterator.next()
+      const { value, done } = await iterator.next();
 
       if (done) {
-        controller.close()
+        controller.close();
       } else {
-        controller.enqueue(value)
+        controller.enqueue(value);
       }
     },
-  })
+  });
 }
 
 function sleep(time: number) {
   return new Promise((resolve) => {
-    setTimeout(resolve, time)
-  })
+    setTimeout(resolve, time);
+  });
 }
 
 const encoder = new TextEncoder();
@@ -81,23 +87,61 @@ async function* makeIterator(buildTag: string, fileName: string) {
 
   await extractionPromise;
 
-  yield encoder.encode(`[<metal>]Deployment started. Tag is ${buildTag}.[</metal>]`);
+  yield encoder.encode(
+    `[<metal>]Deployment started. Tag is ${buildTag}.[</metal>]`
+  );
 
   yield encoder.encode("[<metal>]Planning build...[</metal>]");
   const { stdout: stdoutJson } = await exec(`nixpacks plan ${tempDirName}`);
   const plan = JSON.parse(stdoutJson) as NixpackPlan;
   // Filtering out "npm-9_x" because for some reason it's not listed on:
   // https://search.nixos.org/packages
-  plan.phases.setup.nixPkgs =
-    plan.phases.setup.nixPkgs.filter(nixPackage => nixPackage !== "npm-9_x");
-  writeFileSync(`${tempDirName}/build-plan.json`, JSON.stringify(plan, null, 2), "utf8");
+  plan.phases.setup.nixPkgs = plan.phases.setup.nixPkgs.filter(
+    (nixPackage) => nixPackage !== "npm-9_x"
+  );
+  writeFileSync(
+    `${tempDirName}/build-plan.json`,
+    JSON.stringify(plan, null, 2),
+    "utf8"
+  );
   await sleep(1000);
 
   yield encoder.encode("[<metal>]Generating Dockerfile...[</metal>]");
-  await exec(`nixpacks build ${tempDirName} --name ${buildTag} --config build-plan.json --out ${tempDirName}`);
-  await exec(`mv ${tempDirName}/.nixpacks/Dockerfile ${tempDirName}/Dockerfile`);
+  await exec(
+    `nixpacks build ${tempDirName} --name ${buildTag} --config build-plan.json --out ${tempDirName}`
+  );
+  await exec(
+    `mv ${tempDirName}/.nixpacks/Dockerfile ${tempDirName}/Dockerfile`
+  );
 
   yield encoder.encode("[<metal>]Building OCI image...[</metal>]");
+  const { username, password } = pullUserDockerCredentials();
+  const hasCustomDockerCredentials = username && password;
+  if (hasCustomDockerCredentials) {
+    const dockerLoginStream = spawn("docker", [
+      "--config",
+      `${tempDirName}/.docker`,
+      "login",
+      "-u",
+      username,
+      "--password-stdin",
+    ]);
+    dockerLoginStream.stdin.end(password);
+    await new Promise<void>((resolve, reject) => {
+      dockerLoginStream.on("error", (err) => {
+        console.error("Error: ", err);
+        reject(err);
+      });
+
+      dockerLoginStream.on("exit", (code) => {
+        if (code !== 0) {
+          reject(new Error(`Docker login failed with code ${code}`));
+          return;
+        }
+        resolve();
+      });
+    });
+  }
   /* NOTE: the build takes place in the cloud, so a copy of the build image is
   not available locally. Using the "--push" flag works because Docker BuildCloud
   can directly export the built image to DockerHub. If you want to use the
@@ -106,20 +150,24 @@ async function* makeIterator(buildTag: string, fileName: string) {
   "--output type=registry". For more details, see:
   https://docs.docker.com/reference/cli/docker/buildx/build/#output
   */
-  const hasCustomDockerConfig = existsSync(`${tempDirName}/.docker/config.json`);
+
+  if (hasCustomDockerCredentials) {
+    await ensureBuilderExists(`${tempDirName}/.docker`);
+  }
+
   const dockerFlags = [
     "buildx",
     "build",
     tempDirName,
     "--builder",
-    cloudBuilderName || "",
+    builderLongName,
     "--tag",
     `${org}/${repo}:${buildTag}`,
     "--push",
   ];
 
-  if (hasCustomDockerConfig) {
-    dockerFlags.unshift("--config", `${tempDirName}/.docker/config.json`);
+  if (hasCustomDockerCredentials) {
+    dockerFlags.unshift("--config", `${tempDirName}/.docker`);
   }
 
   const dockerBuildStream = spawn("docker", dockerFlags);
@@ -178,8 +226,62 @@ export async function POST(request: NextRequest) {
   ) as WritableStream<Uint8Array>;
   await request.body.pipeTo(uploadedTarball);
 
-  const iterator = makeIterator(tag, fileName)
-  const stream = iteratorToStream(iterator)
+  const iterator = makeIterator(tag, fileName);
+  const stream = iteratorToStream(iterator);
 
-  return new Response(stream)
+  return new Response(stream);
+}
+
+const pullUserDockerCredentials = () => {
+  /* TODO: this should read the creds from somewhere, like pre-configured values on
+   the user's Metal account or variables in the user's Metal project.
+  */
+  return {
+    username: "",
+    password: "",
+  };
+};
+
+// Partially inspired by ChatGPT.
+async function ensureBuilderExists(dirWithDockerConfig: string) {
+  // ??Todo: in the future, also check if the calling user is authorised to use the builder.
+  /* Todo: do we need to save this builder name somewhere and associate it with the
+  user/project? Because right now this will create a new builder for each request
+  for a user with custom docker credentials. And I don't think I can use a shared
+  builder because of the image publication that happens after the build.
+   */
+  const { stdout } = await exec(
+    `docker --config ${dirWithDockerConfig} buildx ls`
+  );
+  if (!stdout.includes(builderLongName)) {
+    console.log(`***** METAL: Creating builder ${builderLongName} *****`);
+    const builderCreationStream = spawn("docker", [
+      "--config",
+      dirWithDockerConfig,
+      "buildx",
+      "create",
+      "--driver",
+      "cloud",
+      `${org}/${builderName}`,
+      "--name",
+      builderLongName,
+    ]);
+    await new Promise<void>((resolve, reject) => {
+      builderCreationStream.on("error", (err) => {
+        console.error("Error: ", err);
+        reject(err);
+      });
+      builderCreationStream.on("exit", (code) => {
+        if (code !== 0) {
+          reject(
+            new Error(
+              `Docker builder creation (using buildx) failed with code ${code}`
+            )
+          );
+          return;
+        }
+        resolve();
+      });
+    });
+  }
 }
