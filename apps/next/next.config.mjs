627efef3b3007d5432cc72f3f097a2b90e282200
Rafael <rgarcia2009@gmail.com>
2024-05-27 15:27:37 -0400

    Customize the user button so it links to the dashboard (#50)

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -1,18 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        // speed up builds--will happen in gh action separate from build
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-            // speed up builds--`next lint` will happen in gh actions
-        ignoreDuringBuilds: true,
-    },
-
-    experimental: {
-        // for opentelemetry tracing
-        instrumentationHook: true,
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+      },
+    ],
+  },
+  typescript: {
+    // speed up builds--will happen in gh action separate from build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // speed up builds--`next lint` will happen in gh actions
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // for opentelemetry tracing
+    instrumentationHook: true,
+  }
 };
 
 export default nextConfig;
