565c83818a265423d9b35a77ad029da12352dd60
Folusho Oladipo <fdoladipo@gmail.com>
2024-05-30 14:06:16 +0100

    MET-30: create a billing page

--- apps/next/app/dashboard/settings/[[...slug]]/page.tsx
+++ apps/next/app/dashboard/settings/[[...slug]]/page.tsx
@@ -1,44 +1,28 @@
 "use client";
 
 import { OrganizationProfile } from "@clerk/nextjs";
-
-const DotIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      fill="currentColor"
-    >
-      <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
-    </svg>
-  );
-};
-
-const CustomPage = () => {
-  return (
-    <div>
-      <h1>Custom Organization Profile Page</h1>
-      <p>This is the custom organization profile page</p>
-    </div>
-  );
-};
+import { DollarSign, IceCreamCone } from "lucide-react";
+import BillingPage from "./_profile-pages/billing-page";
 
 const OrganizationProfilePage = () => (
   <OrganizationProfile path="/dashboard/settings" routing="path">
-    {/* You can pass the content as a component */}
     <OrganizationProfile.Page
-      label="Custom Page"
-      labelIcon={<DotIcon />}
-      url="custom-page"
+      label="Billing"
+      labelIcon={<DollarSign className="h-3.5 w-3.5" />}
+      url="billing"
     >
-      <CustomPage />
+      <BillingPage />
     </OrganizationProfile.Page>
 
     {/* You can also pass the content as direct children */}
-    <OrganizationProfile.Page label="Terms" labelIcon={<DotIcon />} url="terms">
+    <OrganizationProfile.Page
+      label="Custom"
+      labelIcon={<IceCreamCone className="h-3.5 w-3.5" />}
+      url="custom"
+    >
       <div>
-        <h1>Custom Terms Page</h1>
-        <p>This is the custom terms page</p>
+        <h1>Custom Organization Profile Page</h1>
+        <p>This is a custom organization profile page</p>
       </div>
     </OrganizationProfile.Page>
   </OrganizationProfile>
