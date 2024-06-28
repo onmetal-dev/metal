b48feaf4a0286ae0913bc12b83acdb01e81a8f4b
Rafael Garcia <rgarcia2009@gmail.com>
2024-06-28 10:24:46 -0400

    example way of instrumenting server + client component (spoiler alert: it sucks)

--- packages/webapp/app/server/user/index.ts
+++ packages/webapp/app/server/user/index.ts
@@ -3,6 +3,7 @@ import {
   User as ClerkUser,
 } from "@clerk/nextjs/server";
 import { Team, User, teams, users } from "@db/schema";
+import * as Sentry from "@sentry/nextjs";
 import { eq } from "drizzle-orm";
 import { mustGetClerkActiveOrg, mustGetClerkUser } from "../clerk";
 import { db } from "../db";
@@ -16,6 +17,23 @@ export async function mustGetUser(): Promise<User> {
     throw new Error("User not found");
   }
   return user;
+}
+
+export type UserInstrumentation = {
+  id: string;
+  email: string;
+};
+
+// instrumentUserServerSide should be called in server components to set up instrumentation metadata for users.
+// It returns the instrumentation that it set up, which can be used to pass to client components the same / consistent information.
+// As of 2024-06-28 this is the recommended way to do instrumentation in nextjs / app router: https://github.com/getsentry/sentry-javascript/discussions/10019
+export function instrumentUserServerSide(user: User): UserInstrumentation {
+  const instrumentation = {
+    id: user.id,
+    email: user.email,
+  };
+  Sentry.setUser(instrumentation);
+  return instrumentation;
 }
 
 export async function mustGetActiveTeam(): Promise<Team> {
